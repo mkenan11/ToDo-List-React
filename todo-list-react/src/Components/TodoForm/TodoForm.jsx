d508ff9d00e5dff7d2a8027c0ba603567ed0977a
mkenan11 <mammedovkenan1117@gmail.com>
2025-04-09 17:11:54 +0400

    first commit

---
+++ todo-list-react/src/Components/TodoForm/TodoForm.jsx
@@ -0,0 +1,8 @@
+export function TodoForm() {
+    return (
+        <form>
+            <input type="text" placeholder="Add a todo" />
+            <button type="submit">Add</button>
+        </form>
+    )
+}

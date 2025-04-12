91d06d539b752227acef2f934e66f4d746c30ed0
mkenan11 <mammedovkenan1117@gmail.com>
2025-04-12 13:04:29 +0400

    sil button

--- todo-list-react/src/Components/Modal/Modal.jsx
+++ todo-list-react/src/Components/Modal/Modal.jsx
@@ -1,14 +1,28 @@
 import "./Modal.css";
 
+import { useContext } from "react";
+import { TodoContext } from "../../Context/TodoContext/TodoContext";
+
 export function Modal({ todo, onClose }) {
   if (!todo) return null;
+
+  const { deleteTodo } = useContext(TodoContext);
+
+  const handleDelete = () => {
+    deleteTodo(todo.id);
+    onClose();
+  };
 
   return (
     <div className="modal-backdrop" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <h2>Tapşırıq Məlumatı</h2>
         <p>{todo.text}</p>
-        <button onClick={onClose}>Bağla</button>
+
+        <div className="modal-buttons">
+          <button onClick={handleDelete}>Sil</button>
+          <button onClick={onClose}>Bağla</button>
+        </div>
       </div>
     </div>
   );
